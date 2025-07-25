db53413959c469ca8317ce61b30ee48a405ba883
traorecheikh <atidiane741@gmail.com>
2025-07-25 20:17:19 +0000

    feat: add validation rules for user details in StudentRequest

--- app/Modules/Student/Requests/StudentRequest.php
+++ app/Modules/Student/Requests/StudentRequest.php
@@ -13,6 +13,14 @@ class StudentRequest extends FormRequest
             'matricule' => ['required'],
             'class_model_id' => ['required', 'exists:class_models'],
             'parent_id' => ['required', 'exists:parents'],
+            'user' => ['required', 'array'],
+            'user.first_name' => ['required', 'string'],
+            'user.last_name' => ['required', 'string'],
+            'user.birthday' => ['required', 'date'],
+            'user.email' => ['required', 'email'],
+            'user.password' => ['required', 'string'],
+            'user.adress' => ['required', 'string'],
+            'user.phone' => ['required', 'string'],
         ];
     }
 
