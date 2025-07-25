1c56144994b01c7a4c97d55a8690376fdb776b9f
traorecheikh <atidiane741@gmail.com>
2025-07-25 19:57:24 +0000

    feat: add initial migrations for users, password reset tokens, and sessions tables

---
+++ database/migrations/2025_07_25_192530_create_student_session.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('student_session', function (Blueprint $table) {
+            $table->foreignId('student_id')->constrained('students');
+            $table->foreignUuid('academic_years')->constrained('academic_years');
+            $table->foreignId('class_model_id')->constrained('classes');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('student_session');
+    }
+};
