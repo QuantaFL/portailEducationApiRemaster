e43b31397a6e0fd2c1feb47b79b19bfeea163702
traorecheikh <atidiane741@gmail.com>
2025-07-25 21:42:11 +0000

    refactor(class): remove unused BelongsTo import

--- app/Modules/ClassModel/Models/ClassModel.php
+++ app/Modules/ClassModel/Models/ClassModel.php
@@ -4,7 +4,6 @@ namespace App\Modules\ClassModel\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ClassModel extends Model
 {
