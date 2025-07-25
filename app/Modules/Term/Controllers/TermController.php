b1441ddcaf1d66126af179fda6db2be3f1ef54f7
RBen19 <bonmerciel@gmail.com>
2025-07-25 18:40:21 +0000

    feat(term): ensure term can only be added to active academic year

--- app/Modules/Term/Controllers/TermController.php
+++ app/Modules/Term/Controllers/TermController.php
@@ -3,9 +3,12 @@
 namespace App\Modules\Term\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Modules\Session\Models\AcademicYear;
+use App\Modules\Session\Models\StatutsSessionEnum;
 use App\Modules\Term\Models\Term;
 use App\Modules\Term\Requests\TermRequest;
 use App\Modules\Term\Ressources\TermResource;
+use Illuminate\Validation\Rule;
 
 class TermController extends Controller
 {
@@ -16,7 +19,25 @@ class TermController extends Controller
 
     public function store(TermRequest $request)
     {
-        return response()->json(new TermResource(Term::create($request->validated())));
+        $academicYear = AcademicYear::find($request->academic_year_id);
+        if( $academicYear->status !== StatutsSessionEnum::EN_COURS){
+            return response()->json([
+                'message' => 'on ne peut ajouter de période à une année inactive.',
+            ], 400);
+        }
+        $exists = Term::where('name', $request->name)
+            ->where('academic_year_id', $request->academic_year_id)
+            ->exists();
+
+        if ($exists) {
+            return response()->json([
+                'message' => 'cette prériode existe déjà pour cette année .',
+            ], 400);
+        }
+
+        $term = Term::create($request->validated());
+
+        return response()->json(new TermResource($term), 201);
     }
 
     public function show(Term $term)
@@ -26,6 +47,16 @@ class TermController extends Controller
 
     public function update(TermRequest $request, Term $term)
     {
+        $exists = Term::where('name', $request->name)
+            ->where('academic_year_id', $request->academic_year_id)
+            ->exists();
+
+        if (!$exists) {
+            return response()->json([
+                'message' => ' aucune  prériode correspondante pour l\'année  recherché.',
+            ], 400);
+        }
+
         $term->update($request->validated());
 
         return response()->json(new TermResource($term));
