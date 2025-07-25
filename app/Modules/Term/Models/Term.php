53a82b08fde19aa726122254b2d2419ac75cf915
traorecheikh <atidiane741@gmail.com>
2025-07-25 21:39:55 +0000

    refactor(term): update fillable attributes and rename session relation to academicYear

--- app/Modules/Term/Models/Term.php
+++ app/Modules/Term/Models/Term.php
@@ -5,7 +5,6 @@ namespace App\Modules\Term\Models;
 use App\Modules\AcademicYear\Models\AcademicYear;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Term extends Model
 {
@@ -14,9 +13,12 @@ class Term extends Model
     protected $fillable = [
         'name',
         'academic_year_id',
+        'start_date',
+        'end_date',
+        'status',
     ];
 
-    public function session(): BelongsTo
+    public function academicYear()
     {
         return $this->belongsTo(AcademicYear::class);
     }
