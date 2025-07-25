ac59dcce853b160221758b3b05a15ae818713359
traorecheikh <atidiane741@gmail.com>
2025-07-25 19:57:28 +0000

    feat: add AcademicYearController with CRUD operations and resource responses

---
+++ app/Modules/AcademicYear/Controllers/AcademicYearController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Modules\AcademicYear\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Modules\Session\Models\AcademicYear;
+use App\Modules\Session\Requests\AcademicYearRequest;
+use App\Modules\Session\Ressources\AcademicYearResource;
+
+class AcademicYearController extends Controller
+{
+    public function index()
+    {
+        return response()->json(AcademicYearResource::collection(AcademicYear::all()));
+    }
+
+    public function store(AcademicYearRequest $request)
+    {
+        return response()->json(new AcademicYearResource(AcademicYear::create($request->validated())));
+    }
+
+    public function show(AcademicYear $session)
+    {
+        return response()->json(new AcademicYearResource($session));
+    }
+
+    public function update(AcademicYearRequest $request, AcademicYear $session)
+    {
+        $session->update($request->validated());
+
+        return response()->json(new AcademicYearResource($session));
+    }
+
+    public function destroy(AcademicYear $session)
+    {
+        $session->delete();
+
+        return response()->json();
+    }
+}
