df9bd7aef2902482fbe1437b164b7b68676b8104
traorecheikh <atidiane741@gmail.com>
2025-07-25 21:40:00 +0000

    refactor(teacher): remove unused imports and specialities relation, update userModel relation type hint

--- app/Modules/Teacher/Models/Teacher.php
+++ app/Modules/Teacher/Models/Teacher.php
@@ -2,11 +2,9 @@
 
 namespace App\Modules\Teacher\Models;
 
-use App\Modules\Subject\Models\Subject;
 use App\Modules\User\Models\UserModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Teacher extends Model
 {
@@ -17,14 +15,8 @@ class Teacher extends Model
         'user_model_id',
     ];
 
-    protected $with = ['userModel'];
-    public function userModel(): BelongsTo
+    public function userModel()
     {
         return $this->belongsTo(UserModel::class);
-    }
-
-    public function specialities()
-    {
-        return $this->hasMany(Subject::class);
     }
 }
