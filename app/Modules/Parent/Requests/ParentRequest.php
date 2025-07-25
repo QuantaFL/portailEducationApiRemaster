b85090d19e1a3a03cd55c80acd282eac1a8e3943
RBen19 <bonmerciel@gmail.com>
2025-07-25 14:33:31 +0000

    feat: add fields to reference user on Parent Request

--- app/Modules/Parent/Requests/ParentRequest.php
+++ app/Modules/Parent/Requests/ParentRequest.php
@@ -9,7 +9,14 @@ class ParentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'user_model_id' => ['required', 'exists:user_models'],
+            'user' => ['required', 'array'],
+            'user.first_name' => ['required', 'string'],
+            'user.last_name' => ['required', 'string'],
+            'user.birthday' => ['required', 'date'],
+            'user.email' => ['required', 'email'],
+            'user.password' => ['required', 'string'],
+            'user.adress' => ['required', 'string'],
+            'user.phone' => ['required', 'string'],
         ];
     }
 
