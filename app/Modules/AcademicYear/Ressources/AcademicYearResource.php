72314619803633474823576b540c9833306dbf45
traorecheikh <atidiane741@gmail.com>
2025-07-25 19:57:34 +0000

    feat: add AcademicYearRequest for validation and AcademicYearResource for API responses

---
+++ app/Modules/AcademicYear/Ressources/AcademicYearResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Modules\AcademicYear\Ressources;
+
+use App\Modules\AcademicYear\Models\AcademicYear;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin AcademicYear */
+class AcademicYearResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'label' => $this->label,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
