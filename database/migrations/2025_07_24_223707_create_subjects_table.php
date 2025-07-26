9403a804ea8f6283d2c5362315011f6453ae4d90
traorecheikh <atidiane741@gmail.com>
2025-07-26 12:45:07 +0000

    refactor(subjects): change coefficient column type from string to integer

--- database/migrations/2025_07_24_223707_create_subjects_table.php
+++ database/migrations/2025_07_24_223707_create_subjects_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
             $table->id();
             $table->string('name');
             $table->string('level');
-            $table->string('coefficient');
+            $table->integer('coefficient');
             $table->timestamps();
         });
     }
