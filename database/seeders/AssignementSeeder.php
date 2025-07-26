181a30442039563312e7d592309292722bf6eecc
traorecheikh <atidiane741@gmail.com>
2025-07-26 15:55:57 +0000

    fix(seeder): update AssignementSeeder to map subjects to classes using explicit class-subject mapping

--- database/seeders/AssignementSeeder.php
+++ database/seeders/AssignementSeeder.php
@@ -7,7 +7,6 @@ use App\Modules\ClassModel\Models\ClassModel;
 use App\Modules\Subject\Models\Subject;
 use App\Modules\Teacher\Models\Teacher;
 use App\Modules\Term\Models\Term;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class AssignementSeeder extends Seeder
@@ -27,19 +26,25 @@ class AssignementSeeder extends Seeder
         }
 
         foreach ($classes as $class) {
-            $subjectQuery = Subject::query();
-            if (str_starts_with($class->level, 'Lycée')) {
-                if (str_contains($class->name, 'S')) {
-                    $subjectQuery->where('level', 'Lycée S');
-                } elseif (str_contains($class->name, 'L')) {
-                    $subjectQuery->where('level', 'Lycée L');
-                } else {
-                    $subjectQuery->where('level', $class->level);
-                }
-            } else {
-                $subjectQuery->where('level', $class->level);
+            $subjects = collect();
+            $classSubjectMap = [
+                '6e' => ['Collège'],
+                '5e' => ['Collège'],
+                '4e' => ['Collège'],
+                '3e' => ['Collège'],
+                'Seconde L' => ['Seconde L'],
+                'Seconde S' => ['Seconde S'],
+                'Première L' => ['Première L'],
+                'Première S' => ['Première S'],
+                'Terminale L' => ['Terminale L'],
+                'Terminale S1' => ['Terminale S1'],
+                'Terminale S2' => ['Terminale S2'],
+            ];
+
+            if (isset($classSubjectMap[$class->name])) {
+                $subjectLevels = $classSubjectMap[$class->name];
+                $subjects = Subject::whereIn('level', $subjectLevels)->get();
             }
-            $subjects = $subjectQuery->get(); // Get all subjects for the class level
 
             if ($subjects->isEmpty()) {
                 $this->command->info('No subjects found for class level: ' . $class->level);
@@ -48,7 +53,6 @@ class AssignementSeeder extends Seeder
 
             foreach ($terms as $term) {
                 foreach ($subjects as $subject) {
-                    // Assigner un enseignant aléatoire
                     $teacher = $teachers->random();
 
                     Assignement::create([
