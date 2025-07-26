8c2c900b991d5465644699f21a4eab0720476355
traorecheikh <atidiane741@gmail.com>
2025-07-26 12:13:27 +0000

    refactor(usermodel): add gender field and enforce unique constraint on email

--- database/migrations/2025_07_24_225454_create_user_models_table.php
+++ database/migrations/2025_07_24_225454_create_user_models_table.php
@@ -12,7 +12,8 @@ return new class extends Migration {
             $table->string('first_name');
             $table->string('last_name');
             $table->string('birthday');
-            $table->string('email');
+            $table->enum('gender', ['M','F']);
+            $table->string('email')->unique();
             $table->string('password');
             $table->string('adress');
             $table->string('phone');
