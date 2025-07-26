f8dd60325b0400e28c68c2b54ddb56b537c8f298
RBen19 <bonmerciel@gmail.com>
2025-07-26 00:39:20 +0000

    refac: comment user_model_id on request

--- app/Modules/Parent/Requests/ParentRequest.php
+++ app/Modules/Parent/Requests/ParentRequest.php
@@ -9,7 +9,7 @@ class ParentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'user_model_id' => ['required', 'exists:user_models'],
+           // 'user_model_id' => ['required', 'exists:user_models'],
             'user' => ['required', 'array'],
             'user.first_name' => ['required', 'string'],
             'user.last_name' => ['required', 'string'],
