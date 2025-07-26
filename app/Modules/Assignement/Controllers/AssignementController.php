0ad5500ab6874b3ef37263901e2e90e0d86d8764
RBen19 <bonmerciel@gmail.com>
2025-07-26 19:32:11 +0000

    feat: make align controller

--- app/Modules/Assignement/Controllers/AssignementController.php
+++ app/Modules/Assignement/Controllers/AssignementController.php
@@ -3,10 +3,12 @@
 namespace App\Modules\Assignement\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Modules\AcademicYear\Models\StatusAcademicYearEnum;
 use App\Modules\Assignement\Models\Assignement;
 use App\Modules\Assignement\Requests\AssignementRequest;
 use App\Modules\Assignement\Ressources\AssignementResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AssignementController extends Controller
 {
@@ -17,7 +19,16 @@ class AssignementController extends Controller
 
     public function store(AssignementRequest $request)
     {
+        /*
+         *  'teacher_id' => ['required', 'exists:teachers'],
+            'class_model_id' => ['required', 'exists:class_models'],
+            'subject_id' => ['required', 'exists:subjects'],
+         * */
+        //$currentYear = DB::table("academic_years")
+           // ->where("status",StatusAcademicYearEnum::EN_COURS);
         return response()->json(new AssignementResource(Assignement::create($request->validated())));
+
+
     }
 
     public function show(Assignement $assignement)
