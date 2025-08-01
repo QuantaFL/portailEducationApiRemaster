d56e75abdc22aa0063c42d7124e1c6f7093140e2
traorecheikh <atidiane741@gmail.com>
2025-08-01 21:21:00 +0000

    feat: update DatabaseSeeder to include all necessary seeders and add gender field to User model factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'first_name' => 'Test',
-            'last_name' => 'User',
-            'email' => 'test@example.com',
-        ]);
-
-        $this->call(StudentSessionSeeder::class);
-    }
+        $this->call([
+            RoleModelSeeder::class,
+            AcademicYearSeeder::class,
+            SubjectSeeder::class,
+            TermSeeder::class,
+            UserModelSeeder::class,
+            ParentModelSeeder::class,
+            TeacherSeeder::class,
+            ClassModelSeeder::class,
+            StudentSeeder::class,
+            AssignementSeeder::class,
+            StudentSessionSeeder::class,
+            GradeSeeder::class,
+        ]); }
 }
